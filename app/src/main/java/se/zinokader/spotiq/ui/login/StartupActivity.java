a108dd835eafd35bfd38ac5b8a558dafa4ee6611
Zino Kader <zino@zinokader.se>
2017-05-29 00:08:39 +0200

    Circular reveal animation between activities

--- app/src/main/java/se/zinokader/spotiq/ui/login/StartupActivity.java
+++ app/src/main/java/se/zinokader/spotiq/ui/login/StartupActivity.java
@@ -1,11 +1,17 @@
 package se.zinokader.spotiq.ui.login;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 import com.github.andrewlord1990.snackbarbuilder.SnackbarBuilder;
 
@@ -42,12 +48,57 @@ public class StartupActivity extends BaseActivity<StartupPresenter> {
     }
 
     public void goToAuthentication() {
-        startActivityForResult(new Intent(this, AuthenticationActivity.class), SpotifyConstants.LOGIN_REQUEST_CODE);
+        startActivityForResult(new Intent(this, AuthenticationActivity.class),
+                SpotifyConstants.LOGIN_REQUEST_CODE);
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 
     public void goToLobby() {
-        startActivity(new Intent(this, LobbyActivity.class));
-        binding.logInButton.revertAnimation();
+        Animator activityTransition = getCircularRevealAnimation();
+        activityTransition.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                startActivity(new Intent(getApplicationContext(), LobbyActivity.class));
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                delayResetAnimatedChanges();
+            }
+        });
+        activityTransition.start();
+    }
+
+    private Animator getCircularRevealAnimation() {
+        //calculate bounds
+        int x = binding.root.getWidth() / 2;
+        int y = binding.root.getHeight() / 2;
+        int startRadius = binding.logInButton.getWidth();
+        int endRadius = (int) Math.hypot(binding.root.getWidth(), binding.root.getHeight());
+
+        //hide unwanted views
+        binding.root.setVisibility(View.GONE);
+        binding.logInButton.setVisibility(View.GONE);
+        binding.spotiqLogo.setVisibility(View.GONE);
+
+        Animator circularRevealAnimation = ViewAnimationUtils.createCircularReveal(binding.root,
+                x, y, startRadius, endRadius);
+        circularRevealAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+
+        //prepare root view color
+        binding.root.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent));
+        binding.root.setVisibility(View.VISIBLE);
+
+        return circularRevealAnimation;
+    }
+
+    private void delayResetAnimatedChanges() {
+        new Handler().postDelayed( () -> {
+            binding.logInButton.revertAnimation();
+            binding.root.setVisibility(View.VISIBLE);
+            binding.logInButton.setVisibility(View.VISIBLE);
+            binding.spotiqLogo.setVisibility(View.VISIBLE);
+            binding.root.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),
+                    R.color.colorPrimary));
+        }, 1000);
     }
 
     @Override
