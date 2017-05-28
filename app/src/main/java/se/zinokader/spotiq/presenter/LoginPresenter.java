60e87d7ce15c376d58d3b46310c8827e1bfaaaf2
Zino Kader <zino@zinokader.se>
2017-05-28 02:44:25 +0200

    starting fresh

--- app/src/main/java/se/zinokader/spotiq/presenter/LoginPresenter.java
+++
@@ -1,7 +0,0 @@
-package se.zinokader.spotiq.presenter;
-
-import se.zinokader.spotiq.view.LoginView;
-
-public interface LoginPresenter {
-    void setView(LoginView view);
-}
--- app/src/main/java/se/zinokader/spotiq/view/LoginView.java
+++
@@ -1,9 +0,0 @@
-package se.zinokader.spotiq.view;
-
-import android.app.Activity;
-import android.content.Context;
-import android.view.View;
-
-public interface LoginView {
-    void loginPressed();
-}
