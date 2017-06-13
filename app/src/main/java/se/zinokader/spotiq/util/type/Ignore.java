4fa47bcb2ac3a8aebdf8f8583b4d03d5037eb319
Zino Kader <zino@zinokader.se>
2017-06-13 16:04:10 +0200

    Added documentation

--- app/src/main/java/se/zinokader/spotiq/util/type/Ignore.java
+++ app/src/main/java/se/zinokader/spotiq/util/type/Ignore.java
@@ -1,8 +1,10 @@
 package se.zinokader.spotiq.util.type;
 
+/**
+ * Empty shell-class for use in Observables that don't need to return anything
+ */
 public class Ignore {
 
     public Ignore() {
-
     }
 }
